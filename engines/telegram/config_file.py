e63a26d98b05761bbeb53825a243efcf795a5bc7
edgard <62401294+psded@users.noreply.github.com>
2022-02-12 17:44:19 +0300

    Update config_file.py

--- engines/telegram/config_file.py
+++ engines/telegram/config_file.py
@@ -1,11 +1,9 @@
-# Project token: 1117915604:AAHASBIQXVseruTST1nOd3ARy--w4D6yNwg
-# Qiwi|Test token: 1809099424:AAF9oqmz3IEXpdUmCFArpoiFWiJXJY0PF7w
 import os
-TG_TOKEN = os.getenv('TG_TOKEN') if os.getenv('TG_TOKEN') else "1117915604:AAHASBIQXVseruTST1nOd3ARy--w4D6yNwg"
-host = os.getenv('MYSQL_HOST') if os.getenv('MYSQL_HOST') else "172.19.0.2"
-user = os.getenv('MYSQL_USER') if os.getenv('MYSQL_USER') else "nano"
-passwd = os.getenv('MYSQL_PASS') if os.getenv('MYSQL_PASS') else "NanoBomber13372281337!"
-db = os.getenv('MYSQL_DB') if os.getenv('MYSQL_DB') else "nanobomber"
+TG_TOKEN = os.getenv('TG_TOKEN') if os.getenv('TG_TOKEN') else "telegram token"
+host = os.getenv('MYSQL_HOST') if os.getenv('MYSQL_HOST') else "mysql host"
+user = os.getenv('MYSQL_USER') if os.getenv('MYSQL_USER') else "mysql username"
+passwd = os.getenv('MYSQL_PASS') if os.getenv('MYSQL_PASS') else "mysql password"
+db = os.getenv('MYSQL_DB') if os.getenv('MYSQL_DB') else "mysql database name"
 
 DB_CONF = {"host": host, "user": user, "password": passwd, "db": db}
 DEFAULT_RANK = 0
